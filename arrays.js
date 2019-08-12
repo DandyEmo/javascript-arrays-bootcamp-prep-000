1e8a559e64113bf45d60dd1cad5f40e95d6f39e9
kevin Abreu <kabreu11385@gmail.com>
2019-08-12 19:24:55 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat"];
+
+function addElementToBeginningOfArray(array, element) {
+    return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return  array.slice(array)
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(-1)
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop(array)
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0)
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift(0)
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return [index.splice()]
+}
+  
+
+
+
+
+
+
+
